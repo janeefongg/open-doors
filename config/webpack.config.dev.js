2b4e7bdb0e5d977d1ec3dc6b41d475b881a55ec4
Danny Thai <dthai92@yahoo.com>
2016-10-08 15:29:38 -0700

    :wrench: install enzyme and configure in webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -81,7 +81,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -99,14 +99,15 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
         query: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/react-scripts/
           // directory for faster rebuilds. We use findCacheDir() because of:
           // https://github.com/facebookincubator/create-react-app/issues/483
           cacheDirectory: findCacheDir({
             name: 'react-scripts'
-          })
+          }),
+          presets: ['airbnb']
         }
       },
       // "postcss" loader applies autoprefixer to our CSS.
@@ -146,7 +147,7 @@ module.exports = {
       }
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
