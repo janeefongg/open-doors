ea8dadc12779dba879259eb88cc90a263705c933
Danny Thai <dthai92@yahoo.com>
2016-11-05 12:24:26 -0700

    ✨  add route for search company portal page

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,13 +3,17 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import configureStore from '../configureStore';
 import App from '../App';
+import SearchCompany from './SearchCompany';
 
 const store = configureStore();
 
 const Root = () => (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={App} />
+      <Route path="/" component={App}>
+      </Route>
+      <Route path="/reviews" component={SearchCompany}>
+      </Route>
     </Router>
   </Provider>
 );
