2d7a7da25f10c534eb63e1403344db9109b06bb6
Danny Thai <dthai92@yahoo.com>
2016-11-06 13:23:16 -0800

    🚧  add search input forms and button to search box container

--- src/components/search-company/SearchBoxContainer.js
+++ src/components/search-company/SearchBoxContainer.js
@@ -1,11 +1,29 @@
 // @flow
 import React, { Component } from 'react';
+import SearchInputForm from '../common/SearchInputForm';
+import Button from '../common/Button';
 
 class SearchBoxContainer extends Component {
+
+  handleClick: () => void;
+
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('Clicked!');
+  }
+
   render() {
     return (
       <div>
         SearchBoxContainer
+        <SearchInputForm />
+        <SearchInputForm />
+        <Button handleClick={this.handleClick} text={"Search"} />
       </div>
     );
   }
