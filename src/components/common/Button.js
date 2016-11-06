201c39439ff08f45521265eb79aab36cd7226823
Danny Thai <dthai92@yahoo.com>
2016-11-06 13:24:04 -0800

    🔨 add text prop for button label and modify flow

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,18 +2,21 @@
 import React from 'react';
 
 type Props = {
-  customClass: string,
-  children: Element,
-  handleClick: Function
+  text: string,
+  handleClick: Function,
 }
 
 const Button = (props: Props) => {
-  const buttonClass = () => props.customClass ? "btn " + props.customClass : "btn";
+  const setButtonClass = props.customClass || "btn";
   return (
-    <button type="button" className={buttonClass()} onClick={props.handleClick}>
-      {props.children}
+    <button type="button" className={setButtonClass} onClick={props.handleClick}>
+      {props.text}
     </button>
   )
+}
+
+Button.defaultProps = {
+  customClass: "btn"
 }
 
 export default Button;
