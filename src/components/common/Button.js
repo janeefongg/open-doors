728ff93cf2fea710edee3e90e62010f5345a413c
Danny Thai <dthai92@yahoo.com>
2016-11-06 16:58:53 -0800

    💄  rename click handler prop

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 type Props = {
   text: string,
-  handleClick: Function,
+  handleOnClick: Function,
 }
 
 const Button = (props: Props) => {
   const setButtonClass = props.customClass || "btn";
   return (
-    <button type="button" className={setButtonClass} onClick={props.handleClick}>
+    <button type="button" className={setButtonClass} onClick={props.handleOnClick}>
       {props.text}
     </button>
   )
