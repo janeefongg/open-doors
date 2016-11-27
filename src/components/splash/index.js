a5dbd8360698f4a480b3a7459b8db2c6c31539aa
dthai92 <dthai@spark.net>
2016-11-27 12:29:41 -0800

    ✨  add login form to splash

--- src/components/splash/index.js
+++ src/components/splash/index.js
@@ -1,21 +1,38 @@
 // @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import InputForm from '../common/InputForm';
+import Button from '../common/Button';
 
 class Splash extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Sign in to Open Doors
         </p>
-        <div className="login-container">
-          Login Form goes here
+        <div className="row">
+          <div className="col-xs-offset-4 col-xs-4">
+            <div className="login-form">
+              <div className="row">
+                <div className="col-xs-12">
+                  <div className="row">
+                    <div className="col-xs-12">
+                      <InputForm placeholder='Username' />
+                    </div>
+                    <div className="col-xs-12">
+                      <InputForm placeholder='Password' />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 center-xs">
+                      <Button customClass='lg-btn login-btn' text='Sign in'/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
