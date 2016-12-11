5f879cb5a903c604a4d801e59945ca846c79ec18
Danny Thai <dthai92@yahoo.com>
2016-12-11 01:48:35 -0800

    ✨  add d3 progress circles

--- src/components/company-profile/index.js
+++ src/components/company-profile/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProgressArc from './progress-arc';
 // import './company.scss';
 
 class CompanyProfile extends Component {
@@ -6,6 +7,32 @@ class CompanyProfile extends Component {
     return (
       <div>
         <h1>Company Profile Goes Here</h1>
+        <div className="stats-container">
+          Female
+          <ProgressArc
+            height={150}
+            width={150}
+            innerRadius={50}
+            outerRadius={55}
+            id="d3-arc"
+            duration={1000}
+            backgroundColor="#e6e6e6"
+            foregroundColor="#E338A3"
+            percentComplete={.28}
+          />
+          Male
+          <ProgressArc
+            height={150}
+            width={150}
+            innerRadius={50}
+            outerRadius={55}
+            id="d3-arc"
+            duration={1000}
+            backgroundColor="#e6e6e6"
+            foregroundColor="#E338A3"
+            percentComplete={.78}
+          />
+        </div>
       </div>
     );
   }
