8f570e04230987f9fac3005d074b6984991e60ae
Danny Thai <dthai92@yahoo.com>
2016-12-11 00:02:35 -0800

    🚧  create review page

---
+++ src/components/review/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './review.scss';
+
+class Review extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>My Review</div>
+    );
+  }
+}
+
+export default Review;
