38d85d757624a1ef4d4b721fe8d2adf85ab3e0c2
Kan Adachi <kadachi1023@gmail.com>
2016-12-11 10:45:14 -0800

    :construction: company profile in progress

--- src/components/company-profile/index.js
+++ src/components/company-profile/index.js
@@ -1,73 +1,122 @@
 import React, { Component } from 'react';
 import ProgressArc from './progress-arc';
 import StatBar from './stat-bar';
-// import './company.scss';
+import SingleTitle from '../common/SingleTitle';
+import SingleRating from '../common/SingleRating';
+import './company.scss';
 
 class CompanyProfile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    // set dummy data for now:
+    const companyId = 1;
+    const name = 'Informz';
+    const url = 'www.Informz.com';
+    const address = 'Saratoga Springs, NY';
+    const dummyRatings = {
+      "overallRating"  : 329,
+      "reviewCount"    : 3,
+      "salaryCount"    : 1,
+      "interactions"   : 300,
+      "harassment"     : 433,
+      "advancement"    : 400,
+      "familySupport"  : 400,
+      "workLifeBalance": 333,
+      "equalPay"       : 433,
+    };
+
     return (
-      <div>
-        <h1>Company Profile Goes Here</h1>
-        <div className="stats-container row">
-          Female
-          <ProgressArc
-            height={150}
-            width={150}
-            innerRadius={50}
-            outerRadius={55}
-            id="d3-arc"
-            duration={1000}
-            backgroundColor="#e6e6e6"
-            foregroundColor="#E338A3"
-            percentComplete={.28}
+      <div className="row">
+        <div className="col-xs-10 col-xs-offset-1">
+          <SingleTitle
+            id={companyId}
+            name={name}
+            url={url}
+            address={address}
           />
-          Male
-          <ProgressArc
-            height={150}
-            width={150}
-            innerRadius={50}
-            outerRadius={55}
-            id="d3-arc"
-            duration={1000}
-            backgroundColor="#e6e6e6"
-            foregroundColor="#E338A3"
-            percentComplete={.78}
+          <SingleRating
+            ratings={dummyRatings}
           />
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={341}
-              label="Equal Pay"
-            />
+        </div>
+        <div className="stats-container col-xs-10 col-xs-offset-1">
+          <div className="stats-title col-xs-10 col-xs-offset-1">
+            <h3 className="col-xs-12 stats-label-misc">
+              Stats
+            </h3>
           </div>
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={388}
-              label="Harassment"
-            />
-          </div>
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={480}
-              label="Advancement"
-            />
-          </div>
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={379}
-              label="Family Support"
-            />
-          </div>
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={411}
-              label="Work Life Balance"
-            />
-          </div>
-          <div className="stat-bar-container col-xs-2">
-            <StatBar
-              rating={425}
-              label="Interactions"
-            />
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1 row">
+              <div className="progress-arc col-xs-2">
+                <ProgressArc
+                  height={150}
+                  width={150}
+                  innerRadius={50}
+                  outerRadius={55}
+                  id="d3-arc"
+                  duration={1000}
+                  backgroundColor="#e6e6e6"
+                  foregroundColor="#E338A3"
+                  percentComplete={.28}
+                />
+                <span className="progress-arc-label">Females</span>
+              </div>
+              <div className="progress-arc col-xs-2">
+                <ProgressArc
+                  height={150}
+                  width={150}
+                  innerRadius={50}
+                  outerRadius={55}
+                  id="d3-arc"
+                  duration={1000}
+                  backgroundColor="#e6e6e6"
+                  foregroundColor="#E338A3"
+                  percentComplete={.78}
+                />
+                <span className="progress-arc-label">Male</span>
+              </div>
+
+              <div className="all-stats-container col-xs-7 col-xs-offset-1 row middle-xs">
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={341}
+                    label="Equal Pay"
+                  />
+                </div>
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={388}
+                    label="Harassment"
+                  />
+                </div>
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={480}
+                    label="Advancement"
+                  />
+                </div>
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={379}
+                    label="Family Support"
+                  />
+                </div>
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={411}
+                    label="Work Life Balance"
+                  />
+                </div>
+                <div className="stat-bar-container col-xs-4">
+                  <StatBar
+                    rating={425}
+                    label="Interactions"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/list-companies/single-company/index.js
+++ src/components/list-companies/single-company/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router';
 
-import SingleRating from './SingleRating';
+import SingleRating from '../../common/SingleRating';
+import SingleTitle from '../../common/SingleTitle';
 
 import './index.scss';
 
 const SingleCompany = ({ id, name, url, address, ratings }) => (
   <div className="single-company col-md-12" id={`company-id-${id}`}>
 
-    <div className="company-title row">
-      <div className="company-graphic col-xs-1 col-xs-offset-1">{name[0].toUpperCase()}</div>
-      <div className="company-meta col-xs-7">
-        <h3 className="company-name">
-          <Link to={`/company/${name}/`}>{name}</Link>
-        </h3>
-        <h4 className="company-details">{url} | <span className="company-location">{address}</span></h4>
-      </div>
-      <div className="review-link-container col-xs-2">
-        <Link to={`/company/${id}/review`}>Add a Review</Link>
-      </div>
-    </div>
-
+    <SingleTitle id={id} name={name} url={url} address={address} />
     <SingleRating ratings={ratings} />
 
     <div className="featured-review row">
