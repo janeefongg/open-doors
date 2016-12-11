614c19c1bc5619f31a2306fd36095b63c8022859
Kan Adachi <kadachi1023@gmail.com>
2016-12-10 17:03:41 -0800

    :package: refactors Logo into a component

---
+++ src/components/list/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SearchHeader from '../common/SearchHeader';
+
+class List extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default List;
